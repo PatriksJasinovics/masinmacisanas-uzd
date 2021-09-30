193b792d6d90a25ee62f8b6bc8ad5d35ecd8bc19
PatriksJasinovics <patriks.jasinovics@gmail.com>
2021-09-30 09:49:50 +0300

    jauna funkcija

--- skrapet.py
+++ skrapet.py
@@ -5,7 +5,7 @@ LAPAS = 'lapas/'
 
 rezultats = requests.get(URL)
 if rezultats.status_code == 200:
-    with open('1_lapa.html', 'w', encoding='UTF-8') as f:
+    with open(f"{LAPAS}1_lapa.html", 'w', encoding='UTF-8') as f:
         f.write(rezultats.text)
 else:
     print(f"ERROR: Statusa kods {rezultats.status_code}")
